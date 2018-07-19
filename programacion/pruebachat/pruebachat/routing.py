2205a22d8390da1f53fc08c11cdf022e68e8a895
Jorge <inted00@gmail.com>
2018-07-19 08:43:55 +0200

    primer

---
+++ programacion/pruebachat/pruebachat/routing.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+from chat.consumers import ChatConsumer
+
+application = ProtocolTypeRouter({
+
+    # WebSocket chat handler
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            url(r"^chat/$", ChatConsumer),
+        ])
+    ),
+
+})
