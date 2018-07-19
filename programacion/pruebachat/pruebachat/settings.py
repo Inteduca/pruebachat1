efa94dd650b3619cefb9d4baa892a9e895ff9229
Jorge <inted00@gmail.com>
2018-07-19 09:23:30 +0200

    eee

--- programacion/pruebachat/pruebachat/settings.py
+++ programacion/pruebachat/pruebachat/settings.py
@@ -15,7 +15,15 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "asgi_redis.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+        },
+        
+    },
+}
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
